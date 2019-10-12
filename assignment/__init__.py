9422c53c3581802357d71b2a6fff3363ac0a6325
Balakrishna <balu636216@gmail.com>
2019-10-12 20:29:27 +0700

    No need to edit here

--- assignment/__init__.py
+++ assignment/__init__.py
@@ -1,9 +1,1 @@
-#print(f'Invoking __init__.py for {__name__}')
-import facedet
-from facedet import faceext as abd
-abd.main()
-#import faceext
-#from facedet import faceext 
 
-#main()
-#facedet.faceext.facedet()
