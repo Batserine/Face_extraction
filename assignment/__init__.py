4953956a670d7eb6e757994a1775c1c0a2439ab9
Balakrishna <balu636216@gmail.com>
2019-10-12 18:10:48 +0700

    Initial commit with some changes

---
+++ assignment/__init__.py
@@ -0,0 +1,9 @@
+#print(f'Invoking __init__.py for {__name__}')
+import facedet
+from facedet import faceext as abd
+abd.main()
+#import faceext
+#from facedet import faceext 
+
+#main()
+#facedet.faceext.facedet()
